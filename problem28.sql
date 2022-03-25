265fce36ba58ec367a64efa7b2fb821c7e517813
midorinoyama <fujiaqa@gmail.com>
2022-03-25 23:11:36 +0900

    [Add]problem32.sql

--- problem28.sql
+++ problem28.sql
@@ -1,2 +1,9 @@
 -- 問28.全ての有名人の名前、国名、第一言語を出力
 
+SELECT ce.name, co.name, l.language
+FROM countries AS co
+INNER JOIN countrylanguages AS l
+ON l.country_code = co.code
+AND l.is_official = "T"
+INNER JOIN celebrities AS ce
+ON co.code = ce.country_code;
--- problem29.sql
+++ problem29.sql
@@ -1,1 +1,11 @@
 -- 問29.全ての有名人の名前と国名をテーブル結合せずに出力
+
+SELECT ce.name,
+(SELECT co.name
+FROM countries AS co
+WHERE ce.country_code = co.code) AS "国名"
+FROM celebrities AS ce;
+
+/*SELECT ce.name, co.name AS "国名"
+FROM celebrities AS ce, countries AS co
+WHERE ce.country_code = co.code;*/
