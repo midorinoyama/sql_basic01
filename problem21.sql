a8bb473c5bc3afb30d33066b79db79dc76e4f666
midorinoyama <fujiaqa@gmail.com>
2022-03-27 13:22:47 +0900

    [MODIFY]problem21 スペース

--- problem21.sql
+++ problem21.sql
@@ -1,5 +1,5 @@
 -- 問21.全ての地方の平均寿命、平均人口を表示
 
-SELECT 	region, AVG(life_expectancy) as "平均寿命", AVG(population) as "平均人口"
+SELECT region, AVG(life_expectancy) as "平均寿命", AVG(population) as "平均人口"
 FROM countries
 GROUP BY region;
